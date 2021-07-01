c6aeb6ae5d122a5745c011e7ed3628b2af3ca971
Alan Christie <alan.christie@matildapeak.com>
2021-07-01 12:37:20 +0100

    Initial logic

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # trigger-repository-dispatch
-A Python module to simplify creating GitHub repository dispatch events and payloads
+
+![GitHub tag (latest SemVer)](https://img.shields.io/github/v/tag/informaticsmatters/trigger-repository-dispatch)
+
+[![CodeFactor](https://www.codefactor.io/repository/github/informaticsmatters/trigger-repository-dispatch/badge)](https://www.codefactor.io/repository/github/informaticsmatters/trigger-repository-dispatch)
+
+A Python module to simplify creating GitHub repository dispatch events and
+payloads. Used by some of our GitLab repos to trigger builds in GitHub.
+
+To use in GitLab CI:
+
+```yaml
+env:
+  global:
+  # The origin of the trigger code
+  - TRIGGER_ORIGIN=https://raw.githubusercontent.com/informaticsmatters/trigger-repository-dispatch/2021.1
+
+jobs:
+  include:
+  - stage: trigger
+    script:
+    - curl --location --retry 3 ${TRIGGER_ORIGIN}/requirements.txt --output trigger-repository-dispatch-requirements.txt
+    - curl --location --retry 3 ${TRIGGER_ORIGIN}/trigger-repository-dispatch.py --output trigger-repository-dispatch.py
+    - pip install -r trigger-repository-dispatch-requirements.txt
+    - chmod +x trigger-repository-dispatch.py
+    - ./trigger-repository-dispatch.py EVENT REPO $TOKEN
+```
+
+---
---
+++ trigger-repository-dispatch.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# Trigger a new GitHub build via a repository dispatch event.
+#
+# Add variables using '--vars' where VARS is a comma-separated list of
+# variable names and values, e.g. "A=B,C=D". The variables will be added
+# to the event's client payload.
+#
+# Usage:
+#   trigger-repository-dispatch.py
+#       [--vars VARS]
+#       GITHUB_EVENT
+#       GITHUB_REPO
+#       GITHUB_TOKEN
+#
+# Example:
+#   trigger-repository-dispatch.py \
+#       dm-api informaticsmatters/blob 000 --vars A=1,B=2
+
+import argparse
+import os
+import requests
+
+GITHUB_API_URL = 'https://api.github.com/repos/'
+
+# Build a command-line parser
+# and parse the command-line...
+PARSER = argparse.ArgumentParser(description='Trigger a GitHub Respository Dispatch')
+PARSER.add_argument('event', type=str)
+PARSER.add_argument('repo', type=str)
+PARSER.add_argument('token', type=str)
+PARSER.add_argument('--vars', type=str)
+ARGS = PARSER.parse_args()
+
+# Are GitLab pipeline and tag values present?
+CI_PIPELINE_ID = os.environ.get('CI_PIPELINE_ID')
+CI_COMMIT_TAG = os.environ.get('GI_COMMIT_TAG')
+
+# Any variables to process?
+# True if the length of '--vars' is greater than 2
+# i.e. we have "A=1". "-" is passed in by some scripts to imply none.
+# If there are arguments, split the comma-separated list into a simple list
+VAR_LIST = []
+if ARGS.vars and len(ARGS.vars) > 2:
+    VAR_LIST = ARGS.vars.split(',')
+VARS = {}
+for VAR in VAR_LIST:
+    var = VAR.split('=')
+    assert len(var) == 2
+    VARS[var[0]] = var[1]
+
+# The payload to pass to the GitLab API.
+# We'll add any parameters added by the user
+# and a GitLab pipeline ID and tag if found.
+print(ARGS)
+DATA = {'event_type': ARGS.event}
+CLIENT_PAYLOAD = {}
+# If present
+# adds the CI_PIPELINE_ID and any CI_COMMIT_TAG to the client_payload.
+if CI_PIPELINE_ID:
+    CLIENT_PAYLOAD['CI_PIPELINE_ID'] = CI_PIPELINE_ID
+if CI_COMMIT_TAG:
+    CLIENT_PAYLOAD['GI_COMMIT_TAG'] = CI_COMMIT_TAG
+# Add any variables
+for VAR in VARS:
+    CLIENT_PAYLOAD[VAR] = VARS[VAR]
+# Any client payload?
+if CLIENT_PAYLOAD:
+    DATA['client_payload'] = CLIENT_PAYLOAD
+
+HEADERS = {'Accept': 'Accept: application/vnd.github.v3+json',
+           'Authorization': 'token {}'.format(ARGS.token)}
+URL = 'https://api.github.com/repos/{}/dispatches'.format(ARGS.repo)
+
+# Trigger...
+print(URL)
+response = requests.post(URL, headers=HEADERS, json=DATA, timeout=4.0)
+print(response)
+print(response.text)
